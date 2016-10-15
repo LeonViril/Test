f731d040657040af05f14a423ca2b429b7df4a8b
AsherLago <lactose723@yahoo.com>
2016-10-15 19:30:59 -0400

    First Import

---
+++ Test/Assets/2D Generator/Room.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Room
+{
+    public int xPos;
+    public int yPos;
+    public int roomWidth;
+    public int roomHeight;
+
+    public void SetupRoom(IntRange widthRange, IntRange heightRange, int columns, int rows)
+    {
+        roomWidth = widthRange.Random;
+        roomHeight = heightRange.Random;
+
+        xPos = Mathf.RoundToInt(columns / 2f - roomWidth / 2f);
+        yPos = Mathf.RoundToInt(rows / 2f - roomHeight / 2f);
+    }
